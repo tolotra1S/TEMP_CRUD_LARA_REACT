973bdd902a9ba3b68d376edced07fd2c8698d24e
tolotra1S <tolotraranarison@gmail.com>
2023-02-22 13:25:09 +0100

    first commit

---
+++ crud_lara/app/Http/Controllers/adminController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Admin;
+use Illuminate\Http\Request;
+
+
+class adminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $admins = Admin::all();
+        return response()->json($admins);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $admins = new Admin([
+            'name' => $request->input('name'),
+            'address' => $request->input('address'),
+            'mobile' => $request->input('mobile'),
+ 
+        ]);
+        $admins->save();
+        return response()->json('Admin created!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $contact = Admin::find($id);
+        return response()->json($contact);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $admins = Admin::find($id);
+       $admins->update($request->all());
+       return response()->json('Admin updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $admins = Admin::find($id);
+        $admins->delete();
+        return response()->json(' deleted!');
+    }
+}
---
+++ crud_lara/routes/api.php
@@ -0,0 +1,20 @@
+<?php
+ 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+ 
+use App\Http\Controllers\EmployeeController;
+ 
+ 
+Route::get('/employees',[App\Http\Controllers\EmployeeController::class, 'index']);
+ 
+Route::post('/save',[App\Http\Controllers\EmployeeController::class, 'store']);
+ 
+Route::put('/update/{id}',[App\Http\Controllers\EmployeeController::class, 'update']);
+ 
+Route::delete('/delete/{id}',[App\Http\Controllers\EmployeeController::class, 'destroy']);
+ 
+ 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
